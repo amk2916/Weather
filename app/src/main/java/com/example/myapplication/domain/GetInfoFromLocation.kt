d3c907bbbf8367a227b6cd6327fc011eb6439701
Alexey Kozyrev <tcsid4@gmail.com>
2024-03-26 23:04:11 +0300

    Создал модельку и юзКейсы для первого основной части первого экрана

---
+++ app/src/main/java/com/example/myapplication/domain/GetInfoFromLocation.kt
@@ -0,0 +1,28 @@
+package com.example.myapplication.domain
+
+class GetShortInfo() {
+
+    /**
+     * latitude - широта
+     * longitude - долгота
+     */
+    fun getFromLocation(latitude: Double, longitude: Double) : ShortInfoModel{
+        TODO()
+    }
+
+
+    /**
+     * name - наименование локации в которой необходимо узнать погоду
+     */
+    fun getFromNameLocation(name: String) : ShortInfoModel{
+        TODO()
+    }
+
+    /**
+     * получение списка избранных мест с погодой
+     */
+    fun getInfoInFavoritesLocation() : List<ShortInfoModel>{
+        TODO()
+    }
+
+}
